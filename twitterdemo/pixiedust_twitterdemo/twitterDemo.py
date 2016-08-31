114757789ab5e2675b35e8b33585041568619b5a
vabarbosa <va@us.ibm.com>
2016-08-31 13:07:38 -0400

    first draft

--- twitterdemo/pixiedust_twitterdemo/twitterDemo.py
+++ twitterdemo/pixiedust_twitterdemo/twitterDemo.py
@@ -15,9 +15,49 @@
 # -------------------------------------------------------------------------------
 from pixiedust.display.display import *
 import yaml
+import time
+import json
+import random
+
+BEGINSTREAM = "@BEGINSTREAM@"
+ENDSTREAM = "@ENDSTREAM@"
 
 class PixieDustTwitterDemo(Display):
+  def startStream(self):
+    # TODO: start streaming
+    print(BEGINSTREAM)
+    maxcount = random.randrange(5,25)
+    while True:
+      msg = { \
+        'message': 'streaming :: random > ' + str(maxcount), \
+        'data':[ \
+        { 'key': '#mars', 'value': random.randrange(1,10), 'sadness': random.randrange(1,10), 'joy': random.randrange(1,10), 'fear': random.randrange(1,10) }, \
+        { 'key': '#venus', 'value': random.randrange(1,10), 'sadness': random.randrange(1,10), 'joy': random.randrange(1,10), 'fear': random.randrange(1,10) }, \
+        { 'key': '#earth', 'value': random.randrange(1,10), 'sadness': random.randrange(1,10), 'joy': random.randrange(1,10), 'fear': random.randrange(1,10) }], \
+        'tweets': [{ 'pic': 'http://github.com/DTAIEB.png?size=50', 'name': 'user'+str(random.randrange(1,1000)), 'tweet': 'tweet tweet chirp chirp '+str(maxcount)}]} 
+      print(json.dumps(msg))
+      time.sleep(1)
+      maxcount = maxcount - 1
+      if maxcount < 0:
+        break
+    self.stopStream()
+
+  def stopStream(self):
+    # TODO: stop streaming
+    print(ENDSTREAM)
 
   def doRender(self, handlerId):
     self.addProfilingTime = False
-    self._addHTMLTemplate("startDemo.html")
+    stream = self.options.get("stream")
+
+    if stream is None:
+      self._addHTMLTemplate('demoScript.html',prefix=self.getPrefix())
+      self._addHTMLTemplate('demoPieChart.html',prefix=self.getPrefix())
+      self._addHTMLTemplate('demoGroupedChart.html',prefix=self.getPrefix())
+      self._addHTMLTemplate("demo.html")
+
+    elif stream is True or str(stream).lower() == 'true':
+      self.startStream()
+
+    elif stream is False or str(stream).lower() == 'false':
+      self.stopStream()
