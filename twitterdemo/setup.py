a69dca93880bda8b499e002efe8eda1cdeb0bf74
David Taieb <david_taieb@us.ibm.com>
2016-09-25 23:33:20 -0400

    twitterdemo module configuration

--- twitterdemo/setup.py
+++ twitterdemo/setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(name='pixiedust_twitterdemo',
       version='0.1',
       description='Pixiedust demo of the Twitter Sentiment Analysis tutorials',
-      url='https://github.com/ibm-cds-labs/pixiedust',
+      url='https://github.com/ibm-cds-labs/pixiedust_incubator/tree/master/twitterdemo',
       install_requires=['pixiedust'],
       author='David Taieb',
       author_email='david_taieb@us.ibm.com',
       license='Apache 2.0',
-      packages=[],
+      packages=['pixiedust_twitterdemo'],
+      include_package_data=True,
       zip_safe=False)
